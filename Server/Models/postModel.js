a47db3147da63f93f470336cde7a0f0bed0cc977
Abhijith.M.K <105772028+Abhijith-M-K@users.noreply.github.com>
2022-12-06 10:21:23 +0530

    first commit

---
+++ Server/Models/postModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: { type: String, required: true },
+    desc: String,
+    likes: [],
+    image: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+var PostModel = mongoose.model("Post", postSchema);
+export default PostModel;
