e59da1175037d74768d14e3a432c608afc76e5b7
abhijithmk <abhijithj331@gmail.com>
2023-01-04 12:00:18 +0530

    fix bug

--- Server/Routes/AdminRoute.js
+++ Server/Routes/AdminRoute.js
@@ -1,5 +1,5 @@
 import express from 'express';  
-import { adminLogin, adminRegister} from '../controllers/AdminController.js';
+import { adminLogin, adminRegister} from '../Controllers/AdminController.js';
 
 const router =express.Router();
 
