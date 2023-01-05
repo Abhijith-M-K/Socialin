99df2accac7ac705ba2c5c35f9657555f15622d9
abhijithmk <abhijithj331@gmail.com>
2023-01-05 12:16:07 +0530

    fix db bug

--- Server/index.js
+++ Server/index.js
@@ -62,33 +62,34 @@ const io = new Server(httpServer, {
 let activeUsers = [];
 
 io.on("connection", (socket) => {
-  //add new user
-  socket.on("addNewUser", (newUserId) => {
+  // add new User
+  socket.on("new-user-add", (newUserId) => {
+    // if user is not added previously
     if (!activeUsers.some((user) => user.userId === newUserId)) {
-      activeUsers.push({
-        userId: newUserId,
-        socketId: socket.id,
-      });
+      activeUsers.push({ userId: newUserId, socketId: socket.id });
+      console.log("New User Connected", activeUsers);
     }
-    console.log("Connected users", activeUsers);
-    io.emit("getUsers", activeUsers);
-  });
-
-  //send message
-  socket.on("sendMessage", (data) => {
-    const { receiverId } = data;
-    const receiver = activeUsers.find((user) => user.userId === receiverId);
-    console.log("sending from socket io: ", receiverId);
-    console.log("Data", data);
-    if (receiver) {
-      io.to(receiver.socketId).emit("receiveMessage", data);
-    }
+    // send all active users to new user
+    io.emit("get-users", activeUsers);
   });
 
   socket.on("disconnect", () => {
+    // remove user from active users
     activeUsers = activeUsers.filter((user) => user.socketId !== socket.id);
     console.log("User Disconnected", activeUsers);
-    io.emit("getUsers", activeUsers);
+    // send all active users to all users
+    io.emit("get-users", activeUsers);
+  });
+
+  // send message to a specific user
+  socket.on("send-message", (data) => {
+    const { receiverId } = data;
+    const user = activeUsers.find((user) => user.userId === receiverId);
+    console.log("Sending from socket to :", receiverId)
+    console.log("Data: ", data)
+    if (user) {
+      io.to(user.socketId).emit("recieve-message", data);
+    }
   });
 });
 
